0c8e742a3766028543548a893b856bd0e9c89e53
Safaa <safaaeltom@gmail.com>
2026-05-09 21:44:41 +0300

    Add JavaScript event handling for form submission

---
+++ script.js
@@ -0,0 +1,6 @@
+const emailInput = document.querySelector ("input");
+const submitButton = document.querySelector ("button");
+
+submitButton.addEventListener= ("click", function() {
+    console.log("Button clicked")
+});
