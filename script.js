88692b53a0f5ffa01ed5305254a6ef815a0e215c
Safaa <safaaeltom@gmail.com>
2026-05-09 23:19:04 +0300

    Add empty input validation for email field

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+console.log("JS file loaded");
+
+
 const emailInput = document.querySelector ("input");
 const submitButton = document.querySelector ("button");
 
 submitButton.addEventListener("click", function() {
-    console.log(emailInput.value);
+    const emailValue = emailInput.value;
+    if (emailValue===""){
+    console.log("Email is empty")
+    };
 });
+
+
+
