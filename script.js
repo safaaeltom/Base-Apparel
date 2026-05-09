4bf8b7d730b77ccfc4e0a33dad4fe4eb67c15bbf
Safaa <safaaeltom@gmail.com>
2026-05-09 22:01:12 +0300

    Implement email input handling with JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const emailInput = document.querySelector ("input");
 const submitButton = document.querySelector ("button");
 
-submitButton.addEventListener= ("click", function() {
-    console.log("Button clicked")
+submitButton.addEventListener("click", function() {
+    console.log(emailInput.value);
 });
